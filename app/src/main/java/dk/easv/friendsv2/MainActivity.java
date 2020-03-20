4c39d4217f597096efbe34f416a68a2449bd2e07
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-03-20 14:06:14 +0100

    Broke main activity

--- app/src/main/java/dk/easv/friendsv2/MainActivity.java
+++ app/src/main/java/dk/easv/friendsv2/MainActivity.java
@@ -3,17 +3,21 @@ package dk.easv.friendsv2;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dk.easv.friendsv2.Model.BEFriend;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
     private IDataAccess mDataAccess;
     public static String TAG = "Friend2";
     int SECOND_ACTIVITY = 2;
@@ -21,21 +25,41 @@ public class MainActivity extends ListActivity {
     List<BEFriend> friends;
     ArrayAdapter adapter;
     FriendAdapter friendAdapter;
+    ListView friendList;
+    TextView txt1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setTitle("Friends v2");
+
+        txt1.findViewById(R.id.txt1);
+        friendList.findViewById(R.id.list_friends);
+
+
+        registerForContextMenu(txt1);
 
         mDataAccess = DataAccessFactory.getInstance(this);
         friends = mDataAccess.selectAll();
 
         friendAdapter = new FriendAdapter(this, R.layout.cell , friends);
 
-        setListAdapter(friendAdapter);
+        friendList.setAdapter(friendAdapter);
 
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        Log.d(TAG, "Context menu created ");
 
+        if (v == txt1)
+        {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.context_menu1,menu);
+        }
+    }
+/*
     @Override
     public void onListItemClick(ListView parent, View v, int position,
                                 long id) {
@@ -48,7 +72,7 @@ public class MainActivity extends ListActivity {
         startActivityForResult(x,SECOND_ACTIVITY);
         Log.d(TAG, "Detail activity is started");
     }
-
+*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == SECOND_ACTIVITY) {
@@ -66,7 +90,7 @@ public class MainActivity extends ListActivity {
                             R.layout.cell,
                             friends);
 
-                    setListAdapter(adapter);
+                    friendList.setAdapter(adapter);
 
                     break;
                 case RESULT_CANCELED:
