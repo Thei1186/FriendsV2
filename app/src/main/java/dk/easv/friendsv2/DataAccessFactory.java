2121fed8cf9f5bc26586cf3fff759cd18d2bcd1a
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-04-02 12:56:26 +0200

    Update DataAccessFactory.java

--- app/src/main/java/dk/easv/friendsv2/DataAccessFactory.java
+++ app/src/main/java/dk/easv/friendsv2/DataAccessFactory.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 public class DataAccessFactory {
 
+    // Returns an instance of the SQLite implementation class
     public static IDataAccess getInstance(Context c ) {
         return new SQLiteImpl(c);
     }
